3a8402f5232259e1b242f60d2bfff8b454a9d6de
Gabriel Augusto <gabrielqribeiro28@gmail.com>
2026-03-13 23:28:53 -0300

    Add new variables for car details and penalty

--- src/Teste.java
+++ src/Teste.java
@@ -9,6 +9,7 @@ public class Teste {
         ativo = true; //false
         //Declarar variaveis e atribuir o valor
         char sexo = 'F';
+        String cor = "Prata";
         byte idade = 19;
         short rm = 456;
         int ano = 2026;
@@ -16,6 +17,9 @@ public class Teste {
         float altura = 1.80f; //f no final para dizer que é um float
         double preco = 150.25;
         String nome = "Ricardo";
+        double multa = 450.00;
+        String marca = "VolksWagen";
+        String motor = "16 canecos 2.8";
     }
 
 
