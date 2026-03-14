bb66377eef7a7c7731b7f046753a0b3900fd0c74
Gabriel Augusto <gabrielqribeiro28@gmail.com>
2026-03-13 23:28:15 -0300

    Add Maveric class and new attributes to Carro

--- src/Carro.java
+++ src/Carro.java
@@ -1,12 +1,19 @@
 import java.awt.image.BandCombineOp;
 
 public class Carro {
+   public class Maveric {
 
-    String modelo;
-    int velocidadeMaxima;
-    String cor;
-    double peso;
-    boolean automatico;
-    short ano;
-    float altura;
+    }
+
+
+        String modelo;
+        int velocidadeMaxima;
+        String cor;
+        double peso;
+        boolean automatico;
+        short ano;
+        float altura;
+        String marca;
+        double multa;
+        String motor;
 }
